630e31d9029fed6c3995441fd57f6be629d7b091
Kash <amahlw@gmail.com>
2020-10-08 01:40:53 -0400

    new up

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-const draw_input_container = document.getElementById('draw-input');
+// The headline element
+const display = document.querySelector('#headline-display')
 
-document.getElementById('btn').click(function() {
-    var data = document.getElementById()
-    document.getElementById('display').innerHTML = data
-})
+// The input for size
+const inputSize = document.querySelector('#input-size')
+    // The element that displays the value 
+const showSize = document.querySelector('#show-size')
+    // Repeat this for the input and "show" elements
+    // adding event listener cause thats how it functions 
+inputSize.addEventListener('input', handleInput)
+const inputFont = document.querySelector('#select-font')
+const showFont = document.querySelector('#show-font')
+inputFont.addEventListener('input', handleInput)
+const inputColor = document.querySelector('#input-color')
+const showColor = document.querySelector('#show-color')
+inputColor.addEventListener('input', handleInput)
+const selectedBG = document.querySelector('#input-bg-color')
+const showBGvalue = document.querySelector('#show-bg-color')
+selectedBG.addEventListener('input', handleInput)
+const inputText = document.querySelector('#enter-text')
+const showText = document.querySelector('#headline-display')
+inputText.addEventListener('input', handleInput)
+    // style properties 
+function handleInput() {
+    // font size:
+    const fontSize = inputSize.value + 'px'
+        // style 
+    display.style.fontSize = fontSize
 
-function draw_input() {
-    let infoStr = '';
-    let width_input = document.getElementById('data-input');
-    // let height_input = document.getElementById('box-height');
-    // let color_input = document.getElementById('box-color');
-    infoStr += `<h1>Color</h1> <div style="width:${width_input.value}px; height:${height_input.value}px; background-color:${color_input.value}"></div>`;
-    draw_info_container.innerHTML = infoStr;
+    // size
+    showSize.innerHTML = fontSize
+    const selectedFont = inputFont.value
+    display.style.selectedFont = selectedFont
+    showFont.innerHTML = selectedFont
+    const fontColor = inputColor.value
+    display.style.fontColor = fontColor
+    showColor.innerHTML = fontColor
+    const bgColor = selectedBG.value
+    display.style.bgColor = bgColor
+    showBGvalue.innerHTML = bgColor
+    const userText = inputText.value
+    display.style.inputText = inputText
+    showText.innerHTML = userText
 }
+handleInput();
